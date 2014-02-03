f8f4499f02405d53ea0825f3b01c52be9c0559af
Stephen Compton <sccompton@me.com>
2014-02-03 15:53:46 -0600

    Second commit

--- InstagramClone/CameraViewController.m
+++ InstagramClone/CameraViewController.m
@@ -8,31 +8,61 @@
 
 #import "CameraViewController.h"
 
-@interface CameraViewController ()
+@interface CameraViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
+{
+        NSString *imageName;
+}
 @end
 
 @implementation CameraViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    //    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    //    self.imageView.image = chosenImage;
+    //    [picker dismissViewControllerAnimated:YES completion:NULL];
+    //    chosenImageURL = [info objectForKey:UIImagePickerControllerReferenceURL];
+    
+    
+    [self.navigationController dismissViewControllerAnimated:YES completion:nil];
+    UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+    [selectPhoto setBackgroundImage:image forState:UIControlStateNormal];
+    [selectPhoto setTitle:@"" forState:UIControlStateNormal];
+    
+    NSData *imageData = UIImageJPEGRepresentation(image, 0.1);
+    NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSArray *fileLists = [fileManager contentsOfDirectoryAtPath:path error:nil];
+    int totalCount = 0;
+    for (NSString *fileName in fileLists)
+    {
+        if([[fileName lastPathComponent] hasPrefix:@"image_"]){
+            totalCount++;
+        }
+        NSLog(@"%@",fileLists);
+    }
+    
+    imageName = [NSString stringWithFormat:@"%@/image_%i", path, totalCount];
+    [imageData writeToFile:imageName atomically:YES];
+    
+  //  thumbsNailView.image = image;
+    
 }
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+}
+
+
+
+
+
 
 @end
